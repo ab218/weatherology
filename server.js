c1e47ab386e77a16d9f1a13c7a55253f4b296576
A B <ab218@live.com>
2019-01-17 16:10:54 -0800

    Add basic functionality
    - add cards
    
    - add dark sky api call
    
    - add weather icons

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', (req, res) => res.send('pong'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const weatherRoutes = require('./src/weatherRoutes.js');
+
+app.use('/api/weather', weatherRoutes);
+
+app.listen(process.env.PORT || 8080);
+console.log('server up on 8080');
